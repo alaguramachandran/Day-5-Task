8bc8c8f2b07b337264442b6698d32c29d9229074
alaguramachandran <154115941+alaguramachandran@users.noreply.github.com>
2024-01-09 13:44:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let myResume={
+    "basics": {
+      "name": "Alagu Ramachandran",
+      "email": "alaguramachandran17@gmail.com",
+      "phone": 9940733212,
+      "degree": "B.Tech",
+      "location": {
+        "address": "Pudukkottai",
+        "postalCode": 600002,
+        "city": "Pudukkottai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/alagu-ramachandran-89647829/",
+          "github":"https://github.com/dashboard"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "TKB",
+        "position": "IT Executive",
+        "startDate": "2017-03-10",
+        "endDate": "2024-01-09",
+        "summary": "i am the business executive handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "TAnna University",
+        "department": "IT",
+        "studyType": "fulltime",
+        "batch start year": 2008,
+        "batch end year": 2012,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+        "project": [
+          "automatic attendance maintaing system using python language"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,youtuber,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
